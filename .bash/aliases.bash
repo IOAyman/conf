0669326436690ee647cb6670c3ebe9f83b8f9a81
IOAyman <theycallmethedr@gmail.com>
2016-05-31 18:08:19 +0100

    added a couple aliases

--- .bash/aliases.bash
+++ .bash/aliases.bash
@@ -8,7 +8,5 @@ alias powertop='sudo powertop'
 alias axel='axel -an9'
 alias iotop='sudo iotop'
 alias writeinclipboard='[[ -x `which xclip 2>/dev/null` ]] && cat << EOF | xclip -selection clipboard '
-
-# Conditional
-#[[ -x `which gifi 2>/dev/null` ]] && [[ -x `which npm 2>/dev/null` ]] && alias npm=gifi
-alias khen9oni='while [[ -z `ping -w3 ddg.gg 2>/dev/null` ]]; do sleep 5s; done; notify-send --icon=face-laugh "LHMD" "O2 restored"'
+alias dm='docker-machine'
+alias dc='docker-compose'
--- .bash/profile.bash
+++ .bash/profile.bash
@@ -1,11 +1,11 @@
 # guess im aware of my name or how it is spelled!
 # i want a shorter prompt
-export __BODL_ON='\033[1m'
-export __BOLD_OFF='\033[21m'
-export __ITALIC_ON='\E[3m'
-export __ITALIC_OFF='\E[23m'
-export __COLOR='\[\033[01;36m\]'
-export __COLOR_RESET=`tput sgr0`
+__BODL_ON='\033[1m'
+__BOLD_OFF='\033[21m'
+__ITALIC_ON='\E[3m'
+__ITALIC_OFF='\E[23m'
+__COLOR='\[\033[01;36m\]'
+__COLOR_RESET=`tput sgr0`
 export PS1=${__COLOR}${__BOLD_ON}'\w ▬► '${__BOLD_OFF}${__COLOR_RESET}
 
 # ignore both duplicates as well as lines starting with space
@@ -16,7 +16,7 @@ export HISTIGNORE="ls:ll:clear:cd:cd :..:sudo su:exit*:powertop*:drSetCPUGov*:dr
 
 
 # Load up the rest of config
-[[ `ls ~/.bash/*.bash 2>/dev/null` ]] && for f in `ls ~/.bash/* |grep -v profile.bash`; do . $f; done
+[[ `ls ~/.bash/*.bash 2>/dev/null` ]] && for f in `ls ~/.bash/* |grep -v profile.bash |sort`; do . $f; done
 
 
 [[ -x `which fortune 2>/dev/null` ]] && [[ -x `which cowsay 2>/dev/null` ]] && [[ -z $TMUX ]] && fortune | cowsay
