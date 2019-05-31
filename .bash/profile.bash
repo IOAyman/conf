da942be10634d5d1eca7a51a17c41325e5963cc4
Ayman Nedjmeddine <theycallmethedr@gmail.com>
2019-05-31 12:01:14 +0100

    Update bash prompt and history settings

--- .bash/profile.bash
+++ .bash/profile.bash
@@ -1,17 +1,17 @@
 # https://wiki.archlinux.org/index.php/Bash/Prompt_customization#Common_capabilities
-__BODL_ON=
+__BODL_ON=`tput bold`
 __BOLD_OFF=
-__ITALIC_ON='\E[3m'
-__ITALIC_OFF='\E[23m'
-__COLOR='\[\033[01;36m\]'
-__COLOR_RESET='\[\033[00;00m\]'
+__ITALIC_ON=`tput sitm`
+__ITALIC_OFF=`tput ritm`
+__COLOR=`tput setaf 220`
+__COLOR_RESET=`tput sgr0`
 export PS1=${__COLOR}${__BOLD_ON}'`[[ $ISSERVER ]] && echo "[\h] "`\w`[[ -z "$(which __docker_machine_ps1 2>/dev/null)" ]] || __docker_machine_ps1` ▬► '${__BOLD_OFF}${__COLOR_RESET}
 
 #history
 export HISTFILE=~/.bash/history
 export HISTCONTROL=ignoreboth
-export HISTIGNORE="l:la:ls:ll:lll:cd:cd :..:sudo su:exit*:powertop*:drupgrade*:sync*:khen9oni"
-export HISTSIZE=2000
+export HISTIGNORE="l:la:ls:ll:lll:cd:cd :..:exit*:drupgrade*:sync*:khen9oni"
+export HISTSIZE=5000
 
 #load config
 [[ `ls ~/.bash/*.bash 2>/dev/null` ]] && for f in `ls ~/.bash/*.bash |grep -v 'profile.bash' |sort`; do . $f; done
